5dba8f99872980c8fecd137cf9a91158b8be784a
Student <example@example.com>
2019-11-08 14:50:05 -0800

    Built pizza order form

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,11 @@ OrderList.prototype.addPizza = function(pizza) { // This calls the setPrice meth
   this.assignId();
 }
 
-function Pizza(size, toppings) { // Constructor. This is the data about a pizza. The size, an integer value (0=small, 1=med, 2=large, 3=xl), the toppings, an array of booleans, true if the topping is selected, an ID, which is filled out later when the pizza is added to the orderList, and a price, which is set to a base of 10, and filled out with additional costs with Pizza.setPrice()
+function Pizza(size, crust, sauce, cheese, toppings) { // Constructor. This is the data about a pizza. The size, an integer value (0=small, 1=med, 2=large, 3=xl), the crust, an int (0 = traditional, 1 = stuffed, 2 = gluen free), the sauce (0 = tomato, 1 = bbq, 2 = alfredo), the cheese (0 = Regular Cheese, 1 = Light Cheese, 2 = Extra Cheese, 3 = three cheese blend), the toppings, an array of booleans, true if the topping is selected, an ID, which is filled out later when the pizza is added to the orderList, and a price, which is set to a base of 10, and filled out with additional costs with Pizza.setPrice()
   this.size = size,
+  this.crust = crust,
+  this.sauce = sauce,
+  this.cheese = cheese,
   this.toppings = toppings,
   this.id,
   this.price = 10;
@@ -31,6 +34,6 @@ Pizza.prototype.setPrice = function() { // Sets the price of a pizza based on th
   })
 }
 // Below are test cases for debugging, they will be deleted
-var pizza = new Pizza(2,[false,true,true]);
+var pizza = new Pizza(2, 1, 1, 1, [false,true,true]);
 var orders = new OrderList();
 orders.addPizza(pizza);
